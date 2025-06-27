61c0ad8ea81c0c1d7a7d1471d7e98cddfec4fb7c
sonuk-07 <skjais04@gmail.com>
2025-06-27 19:57:53 +0545

    Initial commit

---
+++ Arrays/src/ReverseArray.java
@@ -0,0 +1,20 @@
+// ReverseArray.java
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr);
+        System.out.println("Reversed Array: " + Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
+    }
+}
+
