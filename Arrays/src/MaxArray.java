7af1ac4c895beab90964e96690c4b85247576f45
sonuk-07 <skjais04@gmail.com>
2025-06-30 15:49:32 +0545

    max array problems solved

--- Arrays/src/MaxArray.java
+++ Arrays/src/MaxArray.java
@@ -1,2 +1,21 @@
+// MaxArray.java
+import java.util.Arrays;
+
 public class MaxArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int ans = max(arr);
+        System.out.println("The maximum value of Array is : " + ans);
+    }
+
+    static int max(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
 }
