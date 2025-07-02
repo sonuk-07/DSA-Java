ae93366d137c4afd7b103506a0e70119bda22a9d
sonuk-07 <skjais04@gmail.com>
2025-07-02 11:03:11 +0545

    smallest number greater than target

--- Arrays/src/SmallestThanTarget.java
+++ Arrays/src/SmallestThanTarget.java
@@ -1,4 +1,25 @@
 // https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
 
+import java.rmi.RMISecurityException;
+
 public class SmallestThanTarget {
+    public static void main(String[] args) {
+        char[] letters = {'x','x','y','y'};
+        char target = 'c';
+        char ans = nextGreatestLetter(letters,target);
+        System.out.println(ans);
+    }
+    static char nextGreatestLetter(char[] letters,char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
 }
