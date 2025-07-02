d143fa9e39232a5e9f5982826bc3b9daa3e0101b
sonuk-07 <skjais04@gmail.com>
2025-07-02 12:58:03 +0545

    first and last postition

--- Arrays/src/FirstAndLastPosition.java
+++ Arrays/src/FirstAndLastPosition.java
@@ -1,4 +1,44 @@
 /* https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/ */
 
+import java.util.Arrays;
+
 public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target  = 6;
+        int[] postions = searchRange(nums,target);
+        System.out.println(Arrays.toString(postions));;
+
+    }
+    public static int[]  searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int firstPosition = searchPosition(nums,target,true);
+        int lastPosition = searchPosition(nums,target,false);
+        return new int[] {firstPosition,lastPosition};
+    }
+
+    static int searchPosition(int[] nums, int target, boolean isFirst){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] < target){
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            }else {
+                ans = mid;
+                if(isFirst){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
 }
