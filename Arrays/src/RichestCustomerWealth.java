631ec9f7483d0a5d462211cc29704b4005894ab5
sonuk-07 <skjais04@gmail.com>
2025-07-02 10:16:46 +0545

    richest customer wealth

--- Arrays/src/RichestCustomerWealth.java
+++ Arrays/src/RichestCustomerWealth.java
@@ -1,4 +1,22 @@
 /* https://leetcode.com/problems/richest-customer-wealth/description/ */
 
 public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{2,8,7},{7,1,3},{1,9,5}};
+        int ans = maximumWealth(accounts);
+        System.out.println(ans);
+    }
+    static int maximumWealth(int[][] accounts){
+        int max = Integer.MIN_VALUE;
+        for(int[] array : accounts){
+            int wealth = 0;
+            for(int num : array){
+                wealth = wealth + num;
+            }
+            if(wealth > max){
+                max = wealth;
+            }
+        }
+        return max;
+    }
 }
