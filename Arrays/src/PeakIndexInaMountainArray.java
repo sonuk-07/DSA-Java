011ee56935efd8b42cd01f47a0ae4b35683db9e1
sonuk-07 <skjais04@gmail.com>
2025-07-03 16:35:54 +0545

    peak index in mountain array

--- Arrays/src/PeakIndexInaMountainArray.java
+++ Arrays/src/PeakIndexInaMountainArray.java
@@ -1,2 +1,23 @@
+/* https://leetcode.com/problems/peak-index-in-a-mountain-array/ */
+
 public class PeakIndexInaMountainArray {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,1,0};
+        int ans = peakIndexInMountainArray(arr);
+        System.out.println(ans);
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = 0;
+        while (start < end){
+            mid  = start + (end - start)/2;
+            if(arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            }else if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+        }
+        return start;
+    }
 }
